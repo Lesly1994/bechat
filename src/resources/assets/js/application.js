fc1a52ecde680753c65cfd9f5adaff73f9ffdf30
Fouyon Joshua <fouyon.joshua@gmail.com>
2019-07-14 17:58:23 +0200

    Update application.js

--- src/resources/assets/js/application.js
+++ src/resources/assets/js/application.js
@@ -1,31 +1,65 @@
-(() => {
-  let username = prompt("Please type your username");
-  const socket = io.connect();
-  let message = document.getElementById("message"),
-    container = document.getElementById("messages"),
-    send = document.getElementById("send");
+const socket = io();
+let message = document.getElementById("message"),
+  target = document.getElementById("messages"),
+  form = document.getElementById('message-form');
 
-  // Request all messages
-  socket.emit("message:history");
-  socket.on("message:history", event => {
-    console.log(event);
-  });
+firebase.initializeApp({
+  apiKey: "AIzaSyDlRsOtf_KW9lrQi-I6uCMT5O8XTD4AUmc",
+  databaseURL: "https://sandbox-af137.firebaseio.com",
+  authDomain: "sandbox-af137.firebaseapp.com"
+});
 
-  // Handling send clicking
-  send.addEventListener("click", e => {
-    e.preventDefault();
-    socket.emit("message:create", {
-      username,
-      message: message.value
-    });
-    message.value = null;
-  });
+window.onload = () => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
 
-  socket.on("message:created", event => {
-    let li = document.createElement("li");
-    li.innerHTML = event.username + ": " + event.message + " " + event.created;
-    li.classList.add("him");
-    container.appendChild(li);
-    window.scrollTo(0, document.querySelector("html").scrollHeight);
+      socket.emit("message:history");
+      socket.emit("username:update", user.displayName);
+
+      socket.on("username:updated", (username) => {});
+      socket.on('message:history', (messages) => {
+        document.body.classList.remove("d-none");
+        messages.forEach(message => {
+          let entry = document.createElement('li');
+          entry.innerHTML = `[${new Date(message.createdAt).toLocaleTimeString()}] ${message.username}: <i>${message.content}</i>`;
+          target.appendChild(entry);
+
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+      });
+
+      socket.on('message:created', (message) => {
+        let entry = document.createElement('li');
+        entry.innerHTML = `[${new Date(message.createdAt).toLocaleTimeString()}] ${message.username}: <i>${message.content}</i>`;
+        target.appendChild(entry);
+
+        window.scroll({
+          top: document.body.scrollHeight, 
+          behavior: 'smooth'
+        });
+      });
+
+      form.addEventListener("submit", e => {
+        e.preventDefault();
+        socket.emit("message:create", {
+          username: user.displayName,
+          content: message.value
+        });
+
+        message.value = null;
+      });
+
+    } else {
+      firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider());
+    }
   });
-})();
+};
+
+
+// socket.on("message:created", event => {
+//   let li = document.createElement("li");
+//   li.innerHTML = event.username + ": " + event.message + " " + event.created;
+//   li.classList.add("him");
+//   container.appendChild(li);
+//   window.scrollTo(0, document.querySelector("html").scrollHeight);
+// });
