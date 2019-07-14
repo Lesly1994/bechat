41c66c78fbc40ce41f7ccb5f3742729a8a155bf5
Fouyon Joshua <fouyon.joshua@gmail.com>
2019-07-14 15:55:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const settings = {
+  debug: process.env.DEBUG || false,
+  application: { port: process.env.PORT || 5000 },
+  mongodb: { url: process.env.MONGODB || 'mongodb://localhost:27017/bechat' },
+  websocket: {}
+};
+
 // configure and listen on port: 8080
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-let server = app.listen(8080, () => {
-  console.log(`Server started at http://127.0.0.1:8080`);
+app.use(express.static(__dirname + '/resources'));
+let server = app.listen(settings.application.port, () => {
+  console.log(`Server started at http://127.0.0.1:${settings.application.port}`);
 });
 
 // Require and create websocket server
-let io = require("socket.io")(server);
+let io = require("socket.io")(server, settings.websocket);
 
 // Connect to mongoDB
-mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@becode-shard-00-00-xb6ce.gcp.mongodb.net:27017,becode-shard-00-01-xb6ce.gcp.mongodb.net:27017,becode-shard-00-02-xb6ce.gcp.mongodb.net:27017/bechat?ssl=true&replicaSet=Becode-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true });
+mongoose.connect(settings.mongodb.url, { useNewUrlParser: true });
 
 // Require required files
 require("./routes/application.js")(app);
-require("./routes/websocket.js")(io);
+require("./routes/websocket.js")(io, settings);
